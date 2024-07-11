aeb95961cb0701278ee0c26688b1b38ab0d9b2e2
Amir Aref <amiraref808@gmail.com>
2024-07-11 12:47:42 +0330

    fix: set proxy for get_updates and read from .env
    fix proxy problems
    write .env-sample

--- src/bot.py
+++ src/bot.py
@@ -19,32 +19,27 @@ logging.basicConfig(level= logging.INFO, format= '%(asctime)s - %(message)s')
 # function handler
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
-        text= 'سلام به ربات تلگرامی رهگیری مرسولات پستی شرکت ملی پست ایران خوش آمدید.\n برای رهگیری بسته پستی خود، لطفا کد رهگیری تان را ارسال نمایید.'
+        text= 'سلام به ربات تلگرامی رهگیری مرسولات پستی شرکت ملی پست ایران خوش آمدید.\n برای رهگیری بسته پستی خود، لطفا کد رهگیری تان را ارسال نمایید.',
+        reply_to_message_id=update.message.id
     )   
-    await main()
-
-async def main():
-    code = input("Enter the code : ")
-    async with AsyncClient() as client:
-        try:
-            data = await get_tracking_post(client=client, tracking_code=code)
-            print(data)
-        except TrackingNotFoundError as e:
-            print(e)
-
 
 if __name__ == '__main__':
-    # print('in main')                              
-    app = ApplicationBuilder().token(token= settings.bot_token).proxy(proxy= 'socks5://127.0.0.1:2080').build()
+    # setup bot 
+    app = ApplicationBuilder().token(token= settings.bot_token)
+    # set proxy
+    if settings.proxy_url is not None:
+        app.proxy(proxy= settings.proxy_url)
+        app.get_updates_proxy(get_updates_proxy=settings.proxy_url)
+    # build bot
+    app = app.build()
+    # add handlers
     startHandler = CommandHandler(command= 'start', callback= start)
     app.add_handlers(
-        [
+        (
             startHandler,
-        ]
+        )
     )
 
     logging.info('bot starting')
-    # print('bot running')
     app.run_polling()
 
-    # asyncio.run(main())
--- src/settings.py
+++ src/settings.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     bot_token : str
+    proxy_url : str | None = None
 
     model_config = SettingsConfigDict(case_sensitive=False, env_file=".env")
     
